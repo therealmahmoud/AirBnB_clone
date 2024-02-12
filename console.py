2c6a7b96b1973005c8dde50d3b73809e73f96963
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-02-12 02:33:40 +0200

    “commit”

--- console.py
+++ console.py
@@ -2,8 +2,6 @@
 """Class HBNBComand a program called console.py
 """
 
-import re
-from shlex import split
 import cmd
 from models.base_model import BaseModel
 from models.user import User
@@ -13,24 +11,6 @@ from models.amenity import Amenity
 from models.state import State
 from models.review import Review
 from models import storage
-
-
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
 
 
 class HBNBCommand(cmd.Cmd):
@@ -166,53 +146,33 @@ class HBNBCommand(cmd.Cmd):
         print(c_instance)
 
     def do_update(self, args):
-        """Usage: update <class> <id> <attribute_name> <attribute_value> or
-       <class>.update(<id>, <attribute_name>, <attribute_value>) or
-       <class>.update(<id>, <dictionary>)
-        Update a class instance of a given id by adding or updating
-        a given attribute key/value pair or dictionary."""
-        argl = parse(args)
-        objdict = storage.all()
+        """
+        Update attributes of a specified instance.
 
-        if len(argl) == 0:
+        Usage: update <class_name> <instance_id> <attribute_name>
+        "<attribute_value>"
+        """
+
+        args = args.split()
+        if len(args) == 0:
             print("** class name missing **")
-            return False
-        if argl[0] not in HBNBCommand.__classes:
+        elif args[0] not in HBNBCommand.__allw_cls:
             print("** class doesn't exist **")
-            return False
-        if len(argl) == 1:
+        elif len(args) < 2:
             print("** instance id missing **")
-            return False
-        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
-            print("** no instance found **")
-            return False
-        if len(argl) == 2:
+        elif len(args) < 3:
             print("** attribute name missing **")
-            return False
-        if len(argl) == 3:
-            try:
-                type(eval(argl[2])) != dict
-            except NameError:
-                print("** value missing **")
-                return False
-
-        if len(argl) == 4:
-            obj = objdict["{}.{}".format(argl[0], argl[1])]
-            if argl[2] in obj.__class__.__dict__.keys():
-                valtype = type(obj.__class__.__dict__[argl[2]])
-                obj.__dict__[argl[2]] = valtype(argl[3])
-            else:
-                obj.__dict__[argl[2]] = argl[3]
-        elif type(eval(argl[2])) == dict:
-            obj = objdict["{}.{}".format(argl[0], argl[1])]
-            for k, v in eval(argl[2]).items():
-                if (k in obj.__class__.__dict__.keys() and
-                        type(obj.__class__.__dict__[k]) in {str, int, float}):
-                    valtype = type(obj.__class__.__dict__[k])
-                    obj.__dict__[k] = valtype(v)
-                else:
-                    obj.__dict__[k] = v
-        storage.save()
+        elif len(args) < 4:
+            print("** value missing **")
+        elif len(args) > 4:
+            return
+        else:
+            for value in storage.all().values():
+                if args[1] == value.id:
+                    setattr(value, args[2], args[3])
+                    storage.save()
+                    return
+            print("** no instance found **")
 
 
 if __name__ == "__main__":
