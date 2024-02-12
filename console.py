cc61339629ee2adabe547c6a761c95c67ff2af53
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-02-12 02:22:40 +0200

    “commit”

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """Class HBNBComand a program called console.py
 """
 
+import re
+from shlex import split
 import cmd
 from models.base_model import BaseModel
 from models.user import User
@@ -12,6 +14,24 @@ from models.state import State
 from models.review import Review
 from models import storage
 
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+    
 
 class HBNBCommand(cmd.Cmd):
     """Command line for the airbnb
@@ -151,7 +171,7 @@ class HBNBCommand(cmd.Cmd):
        <class>.update(<id>, <dictionary>)
         Update a class instance of a given id by adding or updating
         a given attribute key/value pair or dictionary."""
-        argl = args.split()
+        argl = parse(arg)
         objdict = storage.all()
 
         if len(argl) == 0:
