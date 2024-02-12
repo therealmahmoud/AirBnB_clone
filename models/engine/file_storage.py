6e7bd3925795d88f784aed244099f70bb506b7aa
therealmahmoud <moha91875460@gmail.com>
2024-02-12 02:15:17 +0000

    UPDATED

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """A file_storage file which serializes instances to
 a JSON file and deserializes JSON file to instances."""
-
 import json
 from models.base_model import BaseModel
 from models.user import User
